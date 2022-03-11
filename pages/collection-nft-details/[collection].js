ce9cbdcb5048e507f637caa9a224d47925678dc3
mehulappinessworld <mehul@appinessworld.com>
2022-03-11 16:38:11 +0530

    Update [colletion] js

--- pages/collection-nft-details/[collection].js
+++ pages/collection-nft-details/[collection].js
@@ -1,6 +1,4 @@
-import { useRouter } from 'next/router';
 import React, { useState } from 'react';
-import NFTComponentDatabase from '../../components/NFTS/NFTComponentDatabase';
 import Loader from '../../components/Common/Loader';
 import Layout from '../../components/Layout/Layout';
 import { useMeQuery } from '../../hooks/Web2/useMeQuery';
@@ -8,12 +6,10 @@ import { useNftOfCollection } from '../../hooks/Web2/useNftOfCollection';
 import RenderNFTInTabs from '../../components/Tabs/RenderNFTInTabs';
 
 const Collection = ({ collection }) => {
-
-  const router = useRouter()
   const { data, isLoading } = useNftOfCollection({
     collection_name: collection
   })
-  const { data: users, isFetched } = useMeQuery()
+  const { data: users } = useMeQuery()
   if (isLoading) {
     return (
       <Loader />
@@ -22,24 +18,24 @@ const Collection = ({ collection }) => {
   return (
     <Layout>
       <>
-        <div className='collection-widget-area pt-70 pb-70'>
+        <div className='collection-widget-area pt-50 pb-70'>
           <div className='container'>
             <div className='row'>
               <>
                 <div className='row justify-content-left'>
-                  <div className=''>
-                    <h2>Created Nft Of Collection {router.query?.collection_name}</h2>
+                  <div className='border border-red'>
+                    <div className='row justify-content-ceneter'>
+                      <div className='pt-4'></div>
+                      <RenderNFTInTabs
+                        openDialogTitle={"Open Now"}
+                        editOrDelete={false}
+                        user={users?.user}
+                        nfts={data?.nfts ? data?.nfts : []}
+                        message="This Collection Don't Have Any NFT"
+                      />
+                    </div>
                   </div>
-                  <hr className=''
-                    style={{ color: '#f14d5d', height: "5px", borderRadius: "20px" }}></hr>
-                  <div className='pb-70'></div>
-                  <RenderNFTInTabs
-                    openDialogTitle={"Open Now"}
-                    editOrDelete={false}
-                    user={users?.user}
-                    nfts={data?.nfts ? data?.nfts : []}
-                    message="This Collection Don't Have Any NFT"
-                  />
+
                 </div>
               </>
             </div>
