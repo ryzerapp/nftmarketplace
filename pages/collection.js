7edbac9b47c69e0822e8a885790c816208af9ebb
mehulappinessworld <mehul@appinessworld.com>
2022-03-11 16:32:49 +0530

    Refactor Collection Code

--- pages/collection.js
+++ pages/collection.js
@@ -1,52 +1,27 @@
 import React, { useState } from 'react';
-import CollectionComponent from '../components/Collection/CollectionComponent';
 import InvolvedArea from '../components/Common/InvolvedArea';
 import Loader from '../components/Common/Loader';
+import RenderCollectionInTabs from '../components/Tabs/RenderCollectionInTabs';
 import { useCollectionByUser } from '../hooks/Web2/useCollectionByUser';
 import { useMeQuery } from '../hooks/Web2/useMeQuery';
 
 const Collection = () => {
-  const { data, isFetched, isLoading } = useCollectionByUser();
+  const { data, isLoading } = useCollectionByUser();
   const { data: users } = useMeQuery()
-  console.log(users?.user?.saved_collection)
   if (isLoading) {
     return (
       <Loader />
     )
   }
-  const CollectionArea = () => {
-    return (
-      <div className='container'>
-        <div className='row justify-content-center'>
-          {data?.collections?.length > 0 ?
-            data?.collections?.map((res) =>
-              (
-              <CollectionComponent
-                collection={res}
-                key={res?.id}
-                savedCollection={users?.user?.saved_collection ? users?.user?.saved_collection : []}
-                liked_collection={users?.user?.liked_collection ? users?.user?.liked_collection : []}
-                user={users?.user}
-              />
-              )
-              ) : <>
-                <div className='container mt-100'>
-                  <div className='row'>
-                    <div className='col-xs-1 section-title  pb-70' align="center">
-                      <h2>You Don't Have Any Collection</h2>
-                    </div>
-                  </div>
-                </div>
-              </>}
-        </div>
-      </div>
-    )
-  }
   return (
     <>
-      <div className='conatainer'>
-        <div className='row pt-70'>
-          <CollectionArea></CollectionArea>
+      <div className='container'>
+        <div className='row justify-content-center  pt-70'>
+          <RenderCollectionInTabs
+            user={users?.user}
+            collections={data?.collections ? data?.collections : []}
+            message="You Don't Have Any Collection"
+          />
         </div>
       </div>
       <InvolvedArea />
