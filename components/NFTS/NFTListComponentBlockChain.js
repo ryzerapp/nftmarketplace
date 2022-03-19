f0384cc8d5eb1b77d1d18c23839c99212b0b1d67
mehulk13899 <mehulkalathiya13899@gmail.com>
2022-03-19 17:48:57 +0530

    Test DeployMent

--- components/NFTS/NFTListComponentBlockChain.js
+++ components/NFTS/NFTListComponentBlockChain.js
@@ -42,6 +42,7 @@ const NFTListComponentBlockChain = ({ brefetch, walletAddressPassed }) => {
     return data;
   };
 
+
   const { data: nftBalance, isLoading, refetch } = useQuery(['USERBlockChainNFTs'], setData, {
     keepPreviousData: true,
     enabled: false
