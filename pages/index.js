40ca024569d45c489594cb4c72c6c5a5199bd27a
Siddharth.Patel <opensiddhu993@gmail.com>
2022-03-20 22:06:21 +0530

    hero page changed to dark-pro

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,21 @@ import BannerArea from '../components/HomeTwo/BannerArea';
 import baseUrl from "../utils/baseUrl";
 import Layout from "../components/Layout/Layout";
 import MainScreen from '../components/ThreeJs/MainScreen'
+import DarkProNew from '../components/HomeTwo/DarkProNew';
 const Index = ({ data, trendingData }) => {
-    return (
-        <Layout>
-            <BannerArea />
-        </Layout>
-    );
+	return (
+		<Layout>
+			{/* <BannerArea /> */}
+			<DarkProNew />
+		</Layout>
+	);
 };
 
 export async function getServerSideProps(context) {
-    const res = await fetch(`${baseUrl}/nfts/getFeaturedArtwork`);
+	const res = await fetch(`${baseUrl}/nfts/getFeaturedArtwork`);
 	const data = await res.json();
 
-    const trendinfRes = await fetch(`${baseUrl}/nfts/getTrendingArtwork`);
+	const trendinfRes = await fetch(`${baseUrl}/nfts/getTrendingArtwork`);
 	const trendingData = await trendinfRes.json();
 
 	if (!data) {
@@ -24,7 +26,7 @@ export async function getServerSideProps(context) {
 	}
 
 	return {
-        props: { data, trendingData }, // will be passed to the page component as props
+		props: { data, trendingData }, // will be passed to the page component as props
 	};
 }
 
