06b41c3dfbe15fce5c34273063a4e91eb83803da
mehulk13899 <mehulkalathiya13899@gmail.com>
2022-03-31 16:06:32 +0530

    Fetch Collection from BlockChain Done

--- pages/collection-nft-details/[collection].js
+++ pages/collection-nft-details/[collection].js
@@ -1,15 +1,64 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import Loader from '../../components/Common/Loader';
 import Layout from '../../components/Layout/Layout';
 import { useMeQuery } from '../../hooks/Web2/useMeQuery';
 import { useNftOfCollection } from '../../hooks/Web2/useNftOfCollection';
 import RenderNFTInTabs from '../../components/Tabs/RenderNFTInTabs';
+import { XMasonry, XBlock } from "react-xmasonry";
+import { useMoralis, useMoralisWeb3Api } from 'react-moralis';
+import NFTMasonry from '../../components/NFTS/NFTMasonry';
+import { useWeb3 } from '../../providers/Web3Context';
+import { useIPFS } from '../../hooks/Web3/useIPFS';
+import { useQuery } from 'react-query';
 
 const Collection = ({ collection }) => {
-  const { data, isLoading } = useNftOfCollection({
-    collection_name: collection
-  })
-  const { data: users } = useMeQuery()
+  // const { data, isLoading } = useNftOfCollection({
+  //   collection_name: collection
+  // })
+  // const { data: users } = useMeQuery()
+
+  const { Moralis, isWeb3Enabled, isAuthenticated } = useMoralis();
+  const { state: { walletAddress, networkId } } = useWeb3();
+  const { resolveLink } = useIPFS();
+  const nftBalanceJson = async (data) => {
+    if (data?.result) {
+      let NFTs = data?.result;
+      for (let NFT of NFTs) {
+        if (NFT?.metadata) {
+          NFT.metadata = JSON.parse(NFT.metadata);
+          NFT.image_url = resolveLink(NFT.metadata?.image);
+        } else if (NFT?.token_uri) {
+          try {
+            await fetch(NFT.token_uri)
+              .then(async (response) => await response.json())
+              .then((data) => {
+                NFT.image_url = resolveLink(data.image);
+              });
+          } catch (error) {
+          }
+        }
+      }
+      return NFTs;
+    }
+  };
+
+  const setData = async () => {
+    const options = { chain: networkId, address: walletAddress, token_address: collection };
+    console.log(options);
+    const polygonNFTs = await Moralis.Web3API.account.getNFTs(options);
+    const data = await nftBalanceJson(polygonNFTs)
+    return data;
+  };
+  const { data: nftBalance, isLoading, refetch } = useQuery([`collectionnft_${collection}`], setData, {
+    keepPreviousData: true,
+    enabled: false
+  });
+
+  useEffect(() => {
+    if (isWeb3Enabled && isAuthenticated)
+      refetch()
+  }, [isWeb3Enabled, networkId, walletAddress]);
+
   if (isLoading) {
     return (
       <Loader />
@@ -20,8 +69,38 @@ const Collection = ({ collection }) => {
       <>
         <div className='collection-widget-area pt-50 pb-70'>
           <div className='container'>
-            <div className='row'>
-              <>
+            <div className='row justify-content-center'>
+              <XMasonry  >
+                {
+                  nftBalance?.map((nft) => (
+                    <XBlock key={nft?.token_uri}>
+                      {
+                        nft?.image_url ? (
+                          <NFTMasonry
+                            nft={nft}
+                          />
+                        ) : null
+                      }
+
+                    </XBlock>
+                  ))}
+              </XMasonry>
+            </div>
+          </div>
+        </div>
+      </>
+    </Layout>
+  );
+};
+
+export async function getServerSideProps(ctx) {
+  const { collection } = ctx.query;
+  return {
+    props: { collection }, // will be passed to the page component as props
+  };
+}
+export default Collection;
+{/* <>
                 <div className='row justify-content-left'>
                   <div className='border border-red'>
                     <div className='row justify-content-ceneter'>
@@ -38,19 +117,4 @@ const Collection = ({ collection }) => {
                   </div>
 
                 </div>
-              </>
-            </div>
-          </div>
-        </div>
-      </>
-    </Layout>
-  );
-};
-
-export async function getServerSideProps(ctx) {
-  const { collection } = ctx.query;
-  return {
-    props: { collection }, // will be passed to the page component as props
-  };
-}
-export default Collection;
+              </> */}
--- pages/collection.js
+++ pages/collection.js
@@ -1,38 +1,83 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { XBlock, XMasonry } from 'react-xmasonry';
-import CollectionComponent from '../components/Collection/CollectionComponent';
-import CollectionDetails from '../components/Collection/CollectionDetails';
 import InvolvedArea from '../components/Common/InvolvedArea';
 import Loader from '../components/Common/Loader';
-import { useCollections } from '../hooks/Web2/useCollections';
+import Layout from '../components/Layout/Layout'
+import { useMoralis } from 'react-moralis';
+import { useWeb3 } from '../providers/Web3Context';
+import { useQuery } from 'react-query';
+import { useRouter } from 'next/router';
+
 
 const Collection = () => {
-  const { data: collections, isLoading } = useCollections();
+  const { Moralis, isWeb3Enabled, isAuthenticated } = useMoralis();
+  const { state: { walletAddress, networkId } } = useWeb3();
+  const router = useRouter()
+  const setData = async () => {
+    const options = { chain: networkId, address: walletAddress };
+    const polygonNFTs = await Moralis.Web3API.account.getNFTs(options);
+    var dataArr = polygonNFTs?.result?.map(item => {
+      return [item.token_address, {
+        token_address: item?.token_address,
+        name: item?.name,
+        symbol: item?.symbol,
+        contract_type: item?.contract_type,
+      }]
+    });
+    var maparr = new Map(dataArr); // create key value pair from array of array
+    return [...maparr.values()];
+  };
+  const { data: collections, isLoading, refetch } = useQuery(['usercollection'], setData, {
+    keepPreviousData: true,
+    enabled: false
+  });
+
+
+  useEffect(() => {
+    if (isWeb3Enabled && isAuthenticated)
+      refetch()
+  }, [isWeb3Enabled, networkId, walletAddress]);
+
   if (isLoading) {
     return (
       <Loader />
     )
   }
+
+
   return (
-    <>
+
+    <Layout>
       <div className='container'>
         <div className='row justify-content-center  pt-70'>
           <XMasonry>
-            {collections?.map((res) => (
-              <XBlock key={res?.id}>
+            {collections?.map((collection) => (
+              <XBlock key={collection?.token_address}>
                 {
-                  < CollectionDetails
-                    collection={res}
-                  />
+                  <div className="article">
+                    <div className='featured-card box-shadow'>
+                      <div className='featured-card-img'>
+                        <button type='button' className='default-btn border-radius-5' onClick={() =>
+                          router.push(`/collection-nft-details/${collection?.token_address}`)}>
+                          Open Collection
+                        </button>
+                      </div>
+                      <div className='content'>
+                        <h3>
+                          <span>{collection?.name}</span>
+                        </h3>
+                        <p>{collection?.contract_type}</p>
+                      </div>
+                    </div>
+                  </div >
                 }
-
               </XBlock>
             ))}
           </XMasonry>
         </div>
       </div>
       <InvolvedArea />
-    </>
+    </Layout>
   );
 };
 
