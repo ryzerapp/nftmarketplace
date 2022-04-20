7f98ddc7704e2e1f2c6d290dd7322b791a149545
Siddharth.Patel <opensiddhu993@gmail.com>
2022-04-20 16:45:07 +0530

    subscribe functionality done

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,6 +1,33 @@
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { ErrorMessage } from "@hookform/error-message";
+import toast from 'react-hot-toast';
+import { useSubscribe } from "../../hooks/Web2/mutations/useSubscribeMutation";
 
+
+const Emailvalidation = yup.object().shape({
+	email: yup.string().email('Invalid email format').required(),
+});
 const Footer = () => {
+
+	const { mutate: createContactus } = useSubscribe()
+	const { register, handleSubmit, reset, formState: { errors }, } = useForm({
+		resolver: yupResolver(Emailvalidation)
+	});
+
+	const onSubmit = async (data) => {
+		//   alert(JSON.stringify(data));
+		await createContactus(data, {
+			onSuccess: async (res) => {
+				toast.success("Thanks for subscribing!");
+				reset();
+			}
+		})
+	};
+
+
 	return (
 		<>
 			<footer className="footer-area pb-70" style={{ marginTop: "0px" }}>
@@ -25,19 +52,34 @@ const Footer = () => {
 								</div>
 								<p style={{ color: 'white' }}>Subscribe to our newsletter</p>
 								<div className="newsletter-area">
-									<form
+									<form onSubmit={handleSubmit(onSubmit)}
 										className="newsletter-form"
 										data-toggle="validator"
-										method="POST"
 									>
 										<input
+											type='text'
+											{...register("email")}
+											name="email"
+											className='form-control'
+											placeholder='e. g. “micwil@gmail.com”'
+										/>
+										<ErrorMessage
+											errors={errors}
+											name="email"
+											render={({ message }) => <p style={{ color: "#f14D5D", padding: 10 }}>{message}</p>}
+										/>
+
+										{/* <input
 											type="email"
 											className="form-control"
 											placeholder="Enter Your Email"
 											name="EMAIL"
 											required
 											autoComplete="off"
-										/>
+										/> */}
+
+
+
 										<button
 											className="subscribe-btn"
 											type="submit"
